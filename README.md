66d8120b7d1eda2f92aed5c60b6dbab3f017256b
Piotr Lewandowski <plewandowski012@gmail.com>
2023-09-30 03:45:07 +0200

    wspaker

---
+++ README.md
@@ -0,0 +1,24 @@
+## wspaker
+single header lib, for packing binary data into weak singal protocols with universal formatting
+
+### example of packing data into ft4/ft8 CQ frame format
+utils/paker_test.cpp
+
+```
+piotr:utils/ (main✗) $ ./paker_test                                                                                                           [2:53:01]
+
+paker format: CQ nQSxxx xxNN
+frame permutations: 2376275200
+frame bitsize ffloor: 31
+specific base: C=1 Q=1  =1 n=2 Q=1 S=1 x=26 x=26 x=26  =1 x=26 x=26 N=10 N=10 
+
+test raw data: eaffffff65bdc3fffa230b0c
+
+encoded output: 
+frame [0]:  CQ 1Q0UZV NG37
+frame [1]:  CQ 1Q1UVI SK38
+frame [2]:  CQ 0Q2SSW AL06
+frame [3]:  CQ 0Q3AAA AA11
+
+decoded back to: eaffffff65bdc3fffa230b0c
+```
---
+++ utils/paker_test.cpp
@@ -0,0 +1,59 @@
+#include <cstdint>
+
+#define __packed
+#include "../paker.hpp"
+#include <iostream>
+#include <string>
+#include <iomanip> // Dla std::setw i std::setfill
+
+using namespace std;
+using namespace Protocol::Paker;
+
+
+struct TRawFrame
+{
+	int32_t s32Lat;
+	int32_t s32Lon;
+	uint16_t u16Alt;
+	uint8_t u8Course;
+	uint8_t u8SomeTelemetry;
+};
+
+CFT4PacketFactory PacketFactory;
+int main()
+{
+	TRawFrame RawFrame = {-22,-3949211, 9210, 11, 12}; 
+	auto const FtFramesCnt = PacketFactory.EncodeRaw((unsigned char*)&RawFrame, sizeof(TRawFrame)*8);
+	cout << "\npaker format: " << CFT4PacketFactory::TFT4Format::GetPattern() << endl;
+	cout << "frame permutations: " << CFT4PacketFactory::TFT4Format::GetMaxPermutations() << endl;
+	cout << "frame bitsize ffloor: " << CFT4PacketFactory::TFT4Format::GetBitSizeFloor() << endl;
+	cout << "specific base: ";;
+
+	const char* pattern = CFT4PacketFactory::TFT4Format::GetPattern();
+	for(int i = 0; i < strlen(pattern); i++)
+	{
+		cout << pattern[i] << "=" << CFT4PacketFactory::TFT4Format::GetBase(pattern[i]) << " ";
+	}
+	cout << endl << endl;
+
+	cout << "test raw data: ";
+
+	 for(int i = 0; i < sizeof(RawFrame); ++i) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(((unsigned char*)&RawFrame)[i]);
+    }
+
+	cout << "\n\nencoded output: \n";
+	for(unsigned int i = 0; i < FtFramesCnt; i++)
+	{
+		cout << "frame [" << i << "]:  " << PacketFactory.GetPacket(i) << endl;
+	}
+
+	cout << "\ndecoded back to: ";
+	unsigned char C8Dupa[sizeof(RawFrame) + 4] = {0};
+	PacketFactory.DecodeFrames(FtFramesCnt, C8Dupa, sizeof(C8Dupa));
+	for(int i = 0; i < sizeof(RawFrame); ++i) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(((unsigned char*)C8Dupa)[i]);
+    }
+
+	 cout << endl;
+}
